35a6f6b1e99ec8a47f8ec7072277d9edb43c2f06
lydiayhuang <lyd.huang@gmail.com>
2016-09-01 20:54:52 -0700

    Add files via upload

---
+++ objectsAssessment.js
@@ -0,0 +1,41 @@
+var myBook = {
+	title : null,
+	cover : null,
+	pages : null,
+	owned : null,
+}
+
+//YOUR CODE HERE
+
+for (var key in myBook){
+	if (myBook.key === null){
+
+		myBook['title'] = "My Autobiography"
+		myBook['cover'] = "paperback"
+		myBook['pages'] = 320
+		myBook['owned'] = true
+	}
+
+
+
+		myBook['inStores'] = false
+
+
+
+
+
+for (var key in myBook) {console.log(key, key[i])}	
+
+
+
+
+
+
+
+
+
+
+//YOUR CODE ENDS HERE
+
+//Uncomment code below to check if you have properly assigned the values.
+//console.log(myBook);
