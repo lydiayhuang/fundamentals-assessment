652faa0ff197e2a4441416d8fe3c526c13c03e17
lydiayhuang <lyd.huang@gmail.com>
2016-09-01 20:54:34 -0700

    Add files via upload

---
+++ arraysAssessment.js
@@ -0,0 +1,18 @@
+var myArray = [ 'I',  'am',  'currently',  'creating',  'your',  'self',  'assessments',  'in',  'a',  'cafe',  'that',  'has',  'a',  'jazz',  'school',  'adjacent',  'to',  'it',  'in',  'Berkeley,',  'California.',  'These',  'words', 'are',  'merely',  'created',  'to',  'supply',  'myArray',  'values,',  'and',  'altogether',  'do',  'not',  'have',  'any',  'greater',  'importance.',  'Have',  'a',  'nice',  'day.' ];
+
+//YOUR CODE HERE
+
+var lastWord = myArray[myArray.length -1 ]
+
+console.log(myArray.length)
+
+console.log(myArray[21]) 
+
+myArray[21] = "sentences"
+
+
+for (var i= 0; i < myArray.length; i++) {
+	console.log(myArray)}
+
+myArray.push("Goodbye");
+
